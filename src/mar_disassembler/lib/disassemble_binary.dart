58e1c03d56ec2e179d7c383007aac0906388fd12
Ethan Lafrenais <elafrenais12@gmail.com>
2018-09-02 21:56:19 -0400

    Disassembler now labels memory references to data

--- src/mar_disassembler/lib/disassemble_binary.dart
+++ src/mar_disassembler/lib/disassemble_binary.dart
@@ -1,8 +1,8 @@
 import 'dart:typed_data';
 
 import 'src/disassembly/disassembly.dart';
-import 'src/functions/label_jumps.dart';
 import 'src/functions/read_binary.dart';
+import 'src/functions/reinterpret_disassembly.dart';
 import 'src/functions/write_disassembly.dart';
 
 String disassembleBinary(Uri uri, Uint8List data) {
@@ -12,8 +12,8 @@ String disassembleBinary(Uri uri, Uint8List data) {
   // Insert a header comment with the original file URI
   lines.insert(0, DisassemblyLine(null, DisassembledComment('Disassembly of $uri'), null));
 
-  // Try to create named labels for jumps
-  labelJumps(lines);
+  // Reinterpret and annotate the disassembly
+  reinterpretDisassembly(lines);
 
   // Write the disassembly to a string
   return writeDisassembly(lines);
---
+++ src/mar_disassembler/lib/src/functions/reinterpret_disassembly.dart
@@ -0,0 +1,232 @@
+import 'package:mar/mar.dart';
+
+import '../disassembly/disassembly.dart';
+
+void reinterpretDisassembly(List<DisassemblyLine> lines) {
+  final reinterpreter = new _Reinterpreter(lines);
+  reinterpreter.reinterpret();
+}
+
+class _Reinterpreter {
+  int _nextJumpLabelIndex = 0;
+  int _nextDataLabelIndex = 0;
+
+  int _i = 0;
+  List<DisassemblyLine> _lines;
+  
+  _Reinterpreter(this._lines)
+    : assert(_lines != null);
+
+  void reinterpret() {
+    for (_i = 0; _i < _lines.length; _i++) {
+      // Label jumps
+      _tryLabelJump();
+
+      // Label memory references
+      _tryLabelOperandMemory(1);
+      _tryLabelOperandMemory(2);
+    }
+  }
+
+  void _tryLabelJump() {
+    final DisassemblyLine line = _lines[_i];
+    final DisassembledContent content = line.content;
+
+    // Ensure line is an instruction
+    if (line.content is! DisassembledInstruction) {
+      return;
+    }
+
+    final DisassembledInstruction instruction = content;
+
+    // Ensure instruction is for a jump to an immediate address
+    if (!_isJumpMnemonic(instruction.mnemonic)) {
+      return;
+    }
+
+    if (instruction.operand1 is! ImmediateOperand) {
+      return;
+    }
+
+    final ImmediateOperand operand = instruction.operand1;
+
+    // Get the jump target address
+    final int address = operand.value;
+    final int addressIndex = _getAddressIndex(address);
+
+    // Ensure the jump-to address is in the binary
+    if (addressIndex == null) {
+      return;
+    }
+
+    // Label the target
+    String label;
+    bool labelReused = false;
+
+    final DisassembledContent targetContent = _lines[addressIndex].content;
+    if (targetContent is DisassembledLabel) {
+      // Re-use label
+      label = targetContent.label;
+      labelReused = true;
+    } else {
+      label = 'jump_${_nextJumpLabelIndex++}';
+    }
+
+    // Replace immediate operand with a label reference
+    _lines[_i] = DisassemblyLine(
+      line.address,
+      DisassembledInstruction(instruction.mnemonic,
+        operand1: LabelOperand(label),
+        operand2: instruction.operand2
+      ),
+      line.rawBytes
+    );
+    
+    if (!labelReused) {
+      // Insert a label at the target
+      _insertLabel(label, addressIndex);
+    }
+  }
+
+  /// [operandIndex] must be either 1 or 2.
+  void _tryLabelOperandMemory(int operandIndex) {
+    assert(operandIndex == 1 || operandIndex == 2);
+
+    final DisassemblyLine line = _lines[_i];
+    final DisassembledContent content = line.content;
+
+    // Ensure line is an instruction
+    if (line.content is! DisassembledInstruction) {
+      return;
+    }
+
+    final DisassembledInstruction instruction = content;
+
+    final InstructionOperand operand = operandIndex == 1
+      ? instruction.operand1
+      : instruction.operand2;
+
+    // Ensure operand is a memory reference
+    if (operand is! MemoryInstructionOperand) {
+      return;
+    }
+
+    final MemoryInstructionOperand memoryOperand = operand;
+
+    // Ensure memory value is an immediate
+    if (memoryOperand.value is! ImmediateOperand) {
+      return;
+    }
+
+    final ImmediateOperand immediate = memoryOperand.value;
+
+    // Get the target address
+    final int address = immediate.value;
+    final int addressIndex = _getAddressIndex(address);
+
+    // Ensure the target address is in the binary
+    if (addressIndex == null) {
+      return;
+    }
+
+    // Label the target
+    String label;
+    bool labelReused = false;
+
+    final DisassembledContent targetContent = _lines[addressIndex].content;
+    if (targetContent is DisassembledLabel) {
+      // Re-use label
+      label = targetContent.label;
+      labelReused = true;
+    } else {
+      label = 'data_${_nextDataLabelIndex++}';
+    }
+
+    // Replace immediate value with a label reference
+    final newOperand = MemoryInstructionOperand(LabelOperand(label),
+      displacement: memoryOperand.displacement
+    );
+
+    _lines[_i] = DisassemblyLine(
+      line.address,
+      DisassembledInstruction(instruction.mnemonic,
+        operand1: operandIndex == 1 ? newOperand : instruction.operand1,
+        operand2: operandIndex == 2 ? newOperand : instruction.operand2
+      ),
+      line.rawBytes
+    );
+    
+    if (!labelReused) {
+      // Insert a label at the target
+      _insertLabel(label, addressIndex);
+    }
+  }
+
+  void _insertLabel(String label, int atAddress) {
+    // Insert a label at the address
+    _lines.insert(atAddress, DisassemblyLine(
+      null, 
+      DisassembledLabel(label),
+      null
+    ));
+  
+    // Skip an index if the inserted line was before
+    // the current line
+    if (atAddress <= _i) {
+      _i++;
+    }
+  }
+
+  /// Gets the index of the line with the specified [address].
+  int _getAddressIndex(int address) {
+    int lastAddress = null;
+    DisassemblyLine lastLine = null;
+
+    // TODO: Implement faster search algorithm
+    for (int i = 0; i < _lines.length; i++) {
+      final DisassemblyLine line = _lines[i];
+
+      if (line.address == address) {
+        if (lastLine.content is DisassembledLabel) {
+          // Re-use label
+          return i - 1;
+        } else {
+          return i;
+        }
+      }
+
+      if (lastAddress != null
+        && line.address != null
+        && lastAddress < address 
+        && line.address > address
+      ) {
+        // Already passed the address, so no line corresponds to it
+        break;
+      }
+
+      lastAddress = line.address;
+      lastLine = line;
+    }
+
+    // ignore: avoid_returning_null
+    return null;
+  }
+
+  bool _isJumpMnemonic(Mnemonic mnemonic) {
+    return mnemonic == Mnemonic.ja
+      || mnemonic == Mnemonic.jc
+      || mnemonic == Mnemonic.jg
+      || mnemonic == Mnemonic.jge
+      || mnemonic == Mnemonic.jl
+      || mnemonic == Mnemonic.jle
+      || mnemonic == Mnemonic.jmp
+      || mnemonic == Mnemonic.jna
+      || mnemonic == Mnemonic.jnc
+      || mnemonic == Mnemonic.jno
+      || mnemonic == Mnemonic.jns
+      || mnemonic == Mnemonic.jnz
+      || mnemonic == Mnemonic.jo
+      || mnemonic == Mnemonic.js
+      || mnemonic == Mnemonic.jz;
+  }
+}
